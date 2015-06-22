03e602879cd6e4a9664a6b0ac9961ffc15ac0e90
Anderson Kovaski <anderkovaski@gmail.com>
2015-06-21 21:44:42 -0300

    - Adicionado validações de presença, unicidade e confirmação (e-mail e senha) para modelo User

--- beerhouse/app/models/user.rb
+++ beerhouse/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates :password, length: {minimum: 8}, allow_nil: true
+	validates :login, :name, :address, :phone_number, :email_confirmation, :password_confirmation, presence: true
+	validates :login, :name, :email, uniqueness: true
+	validates :password, :email, confirmation: true
 end
