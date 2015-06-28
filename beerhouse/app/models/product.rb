7d54091022514de45981fc3d21628f04f87617b2
Cleiton Marques <cmormarques@gmail.com>
2015-06-28 20:40:26 -0300

    retira validação chata de product

--- beerhouse/app/models/product.rb
+++ beerhouse/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
 	validates :quantity_available, :presence => true
 	validates :desc, :presence => true, :length => { :minimum => 10 }
-	validates :price, :presence => true, :length => { :minimum => 4 }
+	validates :price, :presence => true
 end
